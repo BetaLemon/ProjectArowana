2c3749bb9583ca027f784fc9356a93305464922e
Unknown <yoshimcreary@hotmail.com>
2017-03-06 19:38:16 +0100

    added key pickup and hud

---
+++ Assets/Scripts/KeyPickup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPickup : MonoBehaviour {
+
+    public bool hasKey;
+    public GameObject hudkey;
+
+	// Use this for initialization
+	void Start () {
+        hasKey = false;
+        hudkey.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Key")
+        {
+            print(other.gameObject.name);
+            Destroy(other.gameObject);
+            hudkey.SetActive(true);
+        }
+        
+           
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,103 +1,103 @@
-﻿using UnityEngine;
+﻿// Necessary for Unity.
+using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+// Every script is formed by one single class: (it is required that the filename is the same as the class' name, just like in Octave)
+public class PlayerController : MonoBehaviour { // I don't know what MonoBehaviour means, just that Unity generates this automatically.
 
-    [HideInInspector]
-    public bool facingRight = true;
-    [HideInInspector]
-    public bool jump = false;
-    public bool weightModeHeavy = false;
-    public float speed;
-    public float jumpHeight;
+    [HideInInspector]                           // This is used for the variable defined after it to be hidden in Unity's inspector.
+    public bool facingRight = true;             // Boolean used for knowing if the player faces Left or Right, so we can later flip the sprite.
+    [HideInInspector]                           // This is used for the variable defined after it to be hidden in Unity's inspector.
+    public bool jump = false;                   // Boolean that saves whether the player is jumping or not.
+    public bool weightModeHeavy = false;        // Boolean that knows in which Weight Mode the player is in.
+    public float speed;                         // Float that contains the speed at which the player moves horizontally.
+    public float jumpHeight;                    // Float with the height at which the player will be able to jump.
 
-    private bool grounded = false;
-    private Rigidbody2D rb2d;
-    private bool prevWeightMode = false;
+    private bool grounded = false;              // Bool that knows if the player is touching the ground or not, so we avoid infinite jumping.
+    private Rigidbody2D rb2d;                   // RigidBody2D for the player's RB2D. This is useful for position, scale, rotation, etc. of the player.
+    private bool prevWeightMode = false;        // This just saves the mode set for the player's weight in the previous frame.
 
-    private float dist = 1.5f;
-    private Vector2 dir = new Vector2(0,-1);
-    private float hit;
+    private float dist = 1.5f;                  // Maximum distance to the floor, at which the Raycast will check if we are near it.
+    private Vector2 dir = new Vector2(0,-1);    // Direction at which the Raycast has to look. In this is case, this is down (-y).
 
-    public LayerMask layerGround;
-    RaycastHit2D rayCastHit2D = new RaycastHit2D();
+    public LayerMask layerGround;                       // This saves the layer in which the ground is contained. This is 'cause Unity has layers for every object.
+    RaycastHit2D rayCastHit2D = new RaycastHit2D();     // Checks if the Raycast has hit the ground.
 
-    // Use this for initialization
+    // When the game starts, this is initialized:
     void Awake()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>(); // This will contain the player's RB2D.
     }
 
     // Update is called once per frame
     void Update()
     {
-        rayCastHit2D = Physics2D.Raycast(transform.position, dir.normalized, dist, layerGround);
+        rayCastHit2D = Physics2D.Raycast(transform.position, dir.normalized, dist, layerGround);  // Saves the collision with the ground (or the not collision).
         //Debug.Log(rayCastHit2D.collider != null ? rayCastHit2D.collider.gameObject : null, rayCastHit2D.collider != null ? rayCastHit2D.collider.gameObject : null);
-        Debug.DrawRay(transform.position, dir * dist, Color.red, 0.1f);
+        Debug.DrawRay(transform.position, dir * dist, Color.red, 0.1f);     // Draws the Raycast in form of a red line in the "Scene" tab of the Unity Editor.
 
-        grounded = rayCastHit2D.collider != null;
+        grounded = rayCastHit2D.collider != null;   // If the collider that the Raycast detected is null, then the statement will be false, and thus grounded too.
+                                                    // But if the Raycast hit something, it will contain a collider and the statement will be true, and grounded too.
 
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (Input.GetButtonDown("Jump") && grounded)    // If the player hits the "Jump" Button as configured in the Project Settings > Input.
         {
-            jump = true;
-            print("Jumped");
+            jump = true;        // Set true the jumo bool.
+            print("Jumped");    // Print "Jumped" just for debugging purposes. This needs to be removed in the final game.
         }
 
-        if (Input.GetButtonDown("Weight"))
+        if (Input.GetButtonDown("Weight"))          // If the player hits the "Weight" Button as configured in the Project Settings > Input.
         {
-            weightModeHeavy = !weightModeHeavy;
-            print("Weight changed");
+            weightModeHeavy = !weightModeHeavy;     // Inverts the value of the Weight Mode (Light->Heavy / Heavy->Light).
+            print("Weight changed");                // Print "Weight changed" just for debugging purposes. This needs to be removed in the final game.
         }
 
-        if(prevWeightMode != weightModeHeavy)
+        if(prevWeightMode != weightModeHeavy)                   // If the Weight Mode has changed from one frame to the next, then do...
         {
-            if (weightModeHeavy)
+            if (weightModeHeavy)                                // ... if we are in the Heavy mode...
             {
-                rb2d.mass = 15;
-                rb2d.gravityScale = (float)2;
-                //jumpForce = 6000;
-                transform.localScale = new Vector3(1, 1, 1);
+                rb2d.mass = 15;                                 // ... set the mass to 15.
+                rb2d.gravityScale = (float)2;                   // How much the gravity attracts things.
+                transform.localScale = new Vector3(1, 1, 1);    // For debugging purposes sets back the player's size if it has been changed in the else{} stat
             }
-            else
+            else                                                // But if we are in Light mode...
             {
-                rb2d.mass = 5;
-                rb2d.gravityScale = (float)0.7;
-                //jumpForce = 1000;
-                transform.localScale = new Vector3(0.5f, 1, 1);
+                rb2d.mass = 5;                                  // ... we set the mass to 5.
+                rb2d.gravityScale = (float)0.7;                 // This is how much the gravity attracts.
+                transform.localScale = new Vector3(0.5f, 1, 1); // And for debugging purposes me make the player narrower to see when he's in Light mode.
             }
         }
-        prevWeightMode = weightModeHeavy;
+        prevWeightMode = weightModeHeavy;                       // Update the weight mode so we can check in the next frame.
     }
 
-    void FixedUpdate()
+    void FixedUpdate()  // This update is called at the same framerate the game is working at. In general this is recommended for player movement and physics.
     {
         // Move the player horizontally:
-        float h = Input.GetAxis("Horizontal");
-        if (h != 0)
+        float h = Input.GetAxis("Horizontal");      // Saves the input for the player's horizontal movement =>   [left-arrow] is -1 // [right-arrow] is 1 // [neutral] is 0
+        //                                                                                                             <-                    ->                  --
+        if (h != 0) // If the player has moved left or right/the movement is neutral, do...
         {
-            rb2d.velocity = new Vector2(speed * h, rb2d.velocity.y);
+            rb2d.velocity = new Vector2(speed * h, rb2d.velocity.y);    // Change the player's y-axis speed accordingly. If h is negative, the speed too, and inverse too.
         }
 
-        // This makes the sprite flip to the direction the player is looking.
-        if (h > 0 && !facingRight)
-            Flip();
-        else if (h < 0 && facingRight)
-            Flip();
+        // This makes the sprite flip to the direction the player is looking at.
+        if (h > 0 && !facingRight)      // If the player moves right (h = 1) and the sprite is not already looking right, then...
+            Flip();                     // ...flip the sprite!
+        else if (h < 0 && facingRight)  // If the player moves left (h = -1) and the sprite is not already looking left, then...
+            Flip();                     // ...flip the sprite!
 
         // Applies the jump to the player.
-        if (jump)
+        if (jump)   // If jump is true, ergo the player is jumpig. Duh.
         { 
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);
-            jump = false;
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);   // Set the velocity to a 2D Vector that mantains the player's current x-axis movement, but sets the vertical speed to jumpHeight.
+            jump = false;   // Set jump to false, as it has already jumped, and there is no point in repeating that.
         }
     }
 
-
-    void Flip()
+    void Flip()     // As the name indicates, this function flips the sprite vertically. d->b | b->d
     {
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        facingRight = !facingRight;                 // Switches whether it looks right or not.
+        Vector3 theScale = transform.localScale;    // theScale saves the current scale of the player.
+        theScale.x *= -1;                           // This modifies it to switch from left to right or viceversa.
+        transform.localScale = theScale;            // This updates the player's scale to the one we've calculated.
     }
 }
