843863e6d305dba591ca78024cdda69df07cd91f
Unknown <yoshimcreary@hotmail.com>
2017-03-16 11:19:26 +0100

    added wind

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour {
+
+    public bool activated;
+    public bool triggerEnter;
+	// Use this for initialization
+	void Start () {
+        triggerEnter = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (triggerEnter && !GameObject.Find("Player").GetComponent<PlayerController>().weightModeHeavy)
+        {
+            GameObject.Find("Player").GetComponent<PlayerController>().jump = true;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        triggerEnter = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        triggerEnter = false;
+    }
+
+
+}
