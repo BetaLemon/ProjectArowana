f0c3015ac6eb73bf8f67efe21fff1db8223871fb
Mangonels - Dylan Calaf <dylancalaflatham@gmail.com>
2017-03-20 20:33:48 +0100

    eehhh

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -83,49 +83,16 @@ public class PlayerController : MonoBehaviour { // I don't know what MonoBehavio
                     rb2d.mass = 3;                                 // ... set the mass to 15.
                     rb2d.gravityScale = (float)9;                   // How much the gravity attracts things.
                     jumpHeight = 30;
-                    // For debugging purposes sets back the player's size if it has been changed in the else{} stat
-                    if (facingRight)
-                    {
-                        transform.localScale = new Vector3(1, 1, 1);
-                    }
-                    else
-                    {
-                        transform.localScale = new Vector3(-1, 1, 1);
-                    }
                     break;
                 case 1:
                     rb2d.mass = 5;                                  // ... we set the mass to 5.
                     rb2d.gravityScale = (float)0.7;                 // This is how much the gravity attracts.
                     jumpHeight = 15;
-                    // And for debugging purposes me make the player narrower to see when he's in Light mode.
-                    if (facingRight)
-                    {
-                        //dani here, added these transform 1,1 commentary to test the jellybox since this size change won't happen!
-                        transform.localScale = new Vector3(1, 1, 1);
-                        //transform.localScale = new Vector3(0.5f, 0.5f, 1);
-                    }
-                    else
-                    {
-                        transform.localScale = new Vector3(-1, 1, 1);
-                        //transform.localScale = new Vector3(-0.5f, 0.5f, 1);
-                    }
                     break;
                 case 0:
                     rb2d.mass = 5;                                  // ... we set the mass to 5.
                     rb2d.gravityScale = (float)0.7;                 // This is how much the gravity attracts.
                     jumpHeight = 15;
-                    // And for debugging purposes me make the player narrower to see when he's in Light mode.
-                    if (facingRight)
-                    {
-                        //dani here, added these transform 1,1 commentary to test the jellybox since this size change won't happen!
-                        transform.localScale = new Vector3(1, 1, 1);
-                        //transform.localScale = new Vector3(0.5f, 0.5f, 1);
-                    }
-                    else
-                    {
-                        transform.localScale = new Vector3(-1, 1, 1);
-                        //transform.localScale = new Vector3(-0.5f, 0.5f, 1);
-                    }
                     break;
             }
             /*else                                                // But if we are in Light mode...
