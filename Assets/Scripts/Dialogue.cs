b476f15e395a896739f69287077645a856f5e2d9
Mangonels - Dylan Calaf <dylancalaflatham@gmail.com>
2017-03-31 22:16:16 +0200

    Dialogue script base on level 1. (Play on largest possible screen and ress enter to view current functionality)

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -5,44 +5,47 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Text))]
 public class Dialogue : MonoBehaviour
 {
-    private Text _textComponent;
+    private Text _textComponent; //We save the text we want to write to the text component here
 
-    public string[] DialogueStrings;
+    public string[] DialogueStrings; //All possible dialogues we wish to display
 
-    public float SecondsBetweenCharacters = 0.15f;
+    public float SecondsBetweenCharacters = 0.15f; //Delay between character's being shown on the text display.
 
 
 	void Start () {
         _textComponent = GetComponent<Text>();
-        _textComponent.text = "";
+        _textComponent.text = ""; //Emptying text display just for good measure.
 	}
 	
 
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return)) //Press "Enter" key
         {
-            StartCoroutine(DisplayString(DialogueStrings[0]));
+            StartCoroutine(DisplayString(DialogueStrings[0])); //Starts the coroutine for displaying the string with the index we want.
         }
 	}
 
+    //Async thread "IEnumerator for adding characters to the current text being displayed. Because this needs a delay between characters, we are creating an IEnumerator "thread" for it:
     private IEnumerator DisplayString(string stringToDisplay) {
         int stringLength = stringToDisplay.Length;
         int currentCharacterIndex = 0;
 
         _textComponent.text = "";
 
-        while (currentCharacterIndex < stringLength) {
-            _textComponent.text += stringToDisplay[currentCharacterIndex];
-            currentCharacterIndex++;
+        while (currentCharacterIndex < stringLength)
+        {
+            _textComponent.text += stringToDisplay[currentCharacterIndex]; //Adding a character to the display text.
+            currentCharacterIndex++; //Next character please
 
-            if (currentCharacterIndex < stringLength) {
-                yield retrun new WaitForSeconds(SecondsBetweenCharacters);
+            if (currentCharacterIndex < stringLength)
+            {
+                yield return new WaitForSeconds(SecondsBetweenCharacters); //The delay.
             }
             else
             {
                 break;
             }
         }
-        _textComponent.text = "";
+        //_textComponent.text = ""; //Emptying the display text in order to not conflict with the following iteration of characters.
     }
 }
