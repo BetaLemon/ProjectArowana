4ee56c3d4868cbb8e596a4c5f1374e6ec8ca2510
Unknown <yoshimcreary@hotmail.com>
2017-04-27 11:40:22 +0200

    added hubworld

---
+++ Assets/Scripts/hubDOOR.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class hubDOOR : MonoBehaviour {
+
+    // Use this for initialization
+
+    public ImageFader fadeImage;
+    private Animator animator;
+    public int levelDoor;
+    public bool activable;
+    private bool isCollided;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCollided && Input.GetButtonDown("Switch") && activable)
+        {
+            fadeImage.nextScene = levelDoor;
+            animator.SetBool("Open", true);
+            fadeImage.scenefinish = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        isCollided = true;
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+            isCollided = false;
+    }
+}
