ee47f733c76453addaedcb6558c955334a2d0d69
Mangonels - Dylan Calaf <dylancalaflatham@gmail.com>
2017-05-16 22:31:14 +0200

    Recolectables y chorradas en el nivel 1

---
+++ Assets/Scripts/Collectables/StarCollectable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarCollectable : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D other) //Algo ha tocado la estrella
+    {
+
+        if (other.gameObject == PlayerController.instance.gameObject) //Si el objeto que ha causado la colision es Kloe
+        {
+            Destroy(gameObject);
+        }
+    }
+}
