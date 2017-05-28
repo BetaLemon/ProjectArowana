fdcac6d422e4f22bb35e527fce3b2ac770ebe866
Unknown <yoshimcreary@hotmail.com>
2017-05-28 19:45:36 +0200

    intro with no dialogues

---
+++ Assets/Scripts/IntroScripts/IntroManager2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager2 : MonoBehaviour {
+
+    // Use this for initialization
+    float timeInScene;
+    bool dialogueLaunched;
+    public GameObject convI;
+	void Start () {
+        timeInScene = 0.0f;
+        dialogueLaunched = false;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeInScene = Time.time;
+
+        if (timeInScene > 0.2f && !dialogueLaunched)
+        {
+            convI.GetComponent<DialogueLaunch>().StartConv();
+            dialogueLaunched = true;
+        }
+    }
+}
