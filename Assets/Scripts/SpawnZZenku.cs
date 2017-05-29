31c39a494b2a08c03f327227d37b4b9cbb4302a4
Unknown <yoshimcreary@hotmail.com>
2017-05-29 23:01:20 +0200

    added Z to dialogues and above zenkus if possible

---
+++ Assets/Scripts/SpawnZZenku.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnZZenku : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject Z;
+	void Start () {
+        Z.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Z.SetActive(true);
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Z.SetActive(false);
+    }
+}
